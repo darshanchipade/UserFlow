a492917845b4d4995da0093bb80e6a0d042e7465
darshanchipade <dchipade@gmail.com>
2025-12-04 17:25:06 -0800

    Updated merge to main

---
+++ .next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_d89a228a._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Documents/GitHub/UserFlow/UserFlow/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/dca22_12bc0916._.js",
+  "chunks/[root-of-the-server]__1c7d163a._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Documents/GitHub/UserFlow/UserFlow/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
